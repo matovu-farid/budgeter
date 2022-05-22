52d9ddcb11488a36488fce3ab3431e4a31b9096b
matovu-farid <matovu90@gmail.com>
2022-05-22 22:05:46 +0300

    Add associations

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,7 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it { should have_many :categories}
+  it { should have_many :purchases}
+  it {should validate_presence_of :name}
 end
