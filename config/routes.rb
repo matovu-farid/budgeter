37698981d0a53ba16f1d4f384980ad18bf0018ef
matovu-farid <matovu90@gmail.com>
2022-05-24 08:05:26 +0300

    Add trubo false to registrations

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,10 @@ Rails.application.routes.draw do
   resources :splashes
   resources :purchases
   resources :categories
-  devise_for :users
+  devise_for :users, controllers: {
+    registrations: 'users/registrations'
+  }
+
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
